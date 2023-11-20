139dab7315af0dc30df0d78b1d172313f24f270a
김정훈 <j7papa@naver.com>
2023-11-20 14:43:20 +0900

    Revert "Jung hun98"

--- src/modules/isValidAttempNumber.js
+++
@@ -1,8 +0,0 @@
-const isValidAttempNumber = (string) => {
-  const numberExpression = /^[0-9]+$/;
-  const isNumber = numberExpression.test(string);
-
-  return isNumber;
-};
-
-export default isValidAttempNumber;
